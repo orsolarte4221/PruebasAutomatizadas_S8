b1bd1fa66c951626ca7d07a03679879fabf60a58
Sebastian Mascareno Dagnino <s.mascareno@uniandes.edu.co>
2023-05-20 07:13:25 -0700

    Se cambia un escenario de Sebastian

--- Pruebas_Cypress/cypress/e2e/datapool/aleatorio/Pages_Create_Page_Fb_Description_OverLimit.cy.js
+++ Pruebas_Cypress/cypress/e2e/datapool/aleatorio/Pages_Create_Page_Fb_Description_OverLimit.cy.js
@@ -9,12 +9,13 @@ const properties = require('../../properties_sebastian.json')
 
 let page_title = faker.lorem.sentence();
 let page_body = faker.lorem.paragraphs(3);
-let twitter_title = faker.lorem.words(3);
-let twitter_desc = faker.lorem.paragraphs(5);
+let fb_title = faker.lorem.words(3);
+let fb_desc = faker.lorem.paragraphs(5);
 
-twitter_desc = twitter_desc.replace(/[\r\n]+/g, ' ');
 
-twitter_desc = twitter_desc.slice(0,501)
+fb_desc = fb_desc.replace(/[\r\n]+/g, ' ');
+
+fb_desc = fb_desc.slice(0,501)
 
 
 
@@ -32,14 +33,14 @@ describe('Tratar de ingresar sesion con un email registrado y contraseña incorr
       cy.get('textarea[placeholder="Page title"]').type(page_title)
       cy.get('div[data-placeholder="Begin writing your page..."]').type(page_body)
       cy.get(properties.buttons['page settings']).click()
-      cy.get('button[data-test-button="twitter-data"]').click()
-      cy.get('input[name="post-setting-twitter-title"]').type(twitter_title)
-      cy.get('textarea[name="post-setting-twitter-description"]').type(twitter_desc)
-      cy.get('input[name="post-setting-twitter-title"]').click()
+      cy.get('button[data-test-button="facebook-data"]').click()
+      cy.get('input[name="post-setting-og-title"]').type(fb_title)
+      cy.get('textarea[name="post-setting-og-description"]').type(fb_desc)
+      cy.get('input[name="post-setting-og-title"]').click()
       
       
       
-      cy.get('.response').should('contain', 'Twitter Description cannot be longer than 500 characters.')
+      cy.get('.response').should('contain', 'Facebook Description cannot be longer than 500 characters.')
     
     
       
