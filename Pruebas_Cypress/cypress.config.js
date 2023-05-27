6733f20ab7ca79c437445d1e49cb172c23f50efa
David Salamanca <d.salamancaz@uniandes.edu.co>
2023-05-27 11:25:48 -0500

    Integracion nuevas pruebas E2E Aleatorias, Editar Tags

--- Pruebas_Cypress/cypress.config.js
+++ Pruebas_Cypress/cypress.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:2368/ghost/#/signin",
-    setupNodeEvents(on, config) {
-
-    },
     parseSpecialCharSequences: false, 
   },
 });
---
+++ Pruebas_Cypress/cypress/e2e/datapool/aleatorio/Feature_Editar_Tag_NameWithSpecialCharacters.cy.js
@@ -0,0 +1,64 @@
+
+import {faker} from '@faker-js/faker'
+
+function generateHexColor() {
+  const color = faker.datatype.number(0xffffff).toString(16).padStart(6, '0');
+  return color.toUpperCase();
+}
+
+const name = faker.string.symbol(10);
+const descripcion = faker.lorem.sentences(5);
+const color = generateHexColor();
+const slug = faker.word.sample(10)
+
+describe('Editar un Tag', () => {
+  let variables;
+  
+  before(() => {
+    cy.readFile('variables.json').then((content) => {
+      variables = content;
+    });
+
+  });
+   
+    it('Como usuario administrador inicio sesion en Ghost, busco el primer elemento de la lista Tag y lo edito', () => {
+      cy.viewport(1200, 1500);
+      
+      cy.visit(variables.UrlBase + '/#/signin');
+      cy.get('#identification').type(variables.username);
+      cy.get('#password').type(variables.password);
+      cy.wait(2000);
+
+      cy.get('[data-test-button="sign-in"]').click();
+      cy.wait(5000);
+
+      cy.get('[data-test-nav="tags"]').click();
+      cy.wait(5000);
+
+      cy.get('.gh-list-chevron').first().click();;
+      cy.wait(2000);
+
+      cy.get('#tag-name').clear().type(`${name}`);
+      cy.wait(2000);
+
+      cy.get('input[placeholder="15171A"]').clear().type(`${color}`);
+      cy.wait(2000);
+
+      cy.get('#tag-slug').clear().type(`${slug}`);
+      cy.wait(2000);
+
+      cy.get('#tag-description').clear().type(`${descripcion}`);
+      cy.wait(2000);
+
+      cy.get('.gh-btn.gh-btn-primary.gh-btn-icon.ember-view').click();
+      cy.wait(5000);
+
+      cy.get('[data-test-link="tags-back"]').click();
+      cy.wait(5000);
+
+      cy.get('h3.gh-tag-list-name').contains(`${name}`).should('be.visible');
+      cy.wait(2000);
+
+
+    });
+  });
