21c35e5dca0011fcda3e00c3c455430baca92b29
khcastrillon <47218395+khcastrillon@users.noreply.github.com>
2023-05-27 16:10:14 -0500

    Intagración nuevas pruebas E2E pseudo-aleatorias, editar info usuario

---
+++ Pruebas_Cypress/cypress/e2e/datapool/pseudo-aleatorio/Feature_Editar_Miembro_nombre_limite.cy.js
@@ -0,0 +1,51 @@
+import {faker} from '@faker-js/faker'
+
+// Obtener Variables aleatorias de Faker
+const name = faker.name.firstName();
+
+describe('Gestión Miembro', () => {
+    let properties;
+    let variables;
+  
+    before(() => {
+        cy.readFile('variables.json').then((content) => {
+          variables = content;
+        });
+        cy.readFile('post_properties.json').then((content_post) => {
+          properties = content_post;
+        });
+      });
+  
+      it('Hacer Login, crear un miembro, modificar un miembro y validar que el miembro ha sido modificado', () => {
+        cy.visit(variables.UrlBase);
+        cy.wait(2000);
+        cy.get('#identification').type(variables.username);
+        cy.get('#password').type(variables.password);
+        cy.get('[data-test-button=\"sign-in\"]').click();
+        cy.wait(2000);
+
+
+        cy.get("[data-test-nav=\"members\"]").click();
+        cy.wait(2000);
+        cy.get("[data-test-new-member-button=\"true\"]").click();
+        cy.wait(2000);
+        cy.get("[data-test-input=\"member-name\"]").type('Nuevo miembro');
+        cy.get("[data-test-input=\"member-email\"]").type('nuevoeditaado@correo.com');
+        cy.get("[data-test-button=\"save\"]").click();
+        
+        cy.wait(2000);
+        cy.get("[data-test-nav=\"members\"]").click();
+        cy.wait(2000);
+        cy.get(".gh-member-avatar-image").filter((_, element) => {
+          return Cypress.$(element).attr('alt') === 'Nuevo miembro';
+        }).click();
+        cy.wait(2000);
+        cy.get("[data-test-input=\"member-name\"]").clear().type(name);
+        cy.wait(2000);
+        cy.get("[data-test-button=\"save\"]").click();
+        cy.get(properties.buttons["miembro-primero"]).first().click();
+        cy.wait(2000);
+    
+        cy.get(properties.elements["miembro-titulo-nombre"]).should('contain', name);
+      });
+  });
